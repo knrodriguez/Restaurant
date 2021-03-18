c091e75a3ed2853b70c3c421509646ff0828f646
Kirstie Rodriguez <kirstienrodriguez@gmail.com>
2021-03-18 15:59:09 -0400

    first commit

--- models/restaurant.js
+++ models/restaurant.js
@@ -3,10 +3,78 @@ const db = require('./db');
 
 const Restaurant = db.define('restaurant', {
   // Your code here
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate:{
+      notEmpty: true
+    }
+  },
+  cuisineType: {
+    type: Sequelize.ENUM('CHINESE', 'MEXICAN', 'KOREAN', 'INDIAN', 'AMERICAN'),
+    allowNull: false,
+    validate:{
+      notEmpty: true
+    }
+  },
+  rating: {
+    type: Sequelize.FLOAT(2),
+    allowNull: false,
+    validate:{
+      min: 0,
+      max: 5
+    }
+  },
+  numVisits: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 0
+  },
+  description:{
+    type: Sequelize.VIRTUAL,
+    get() {
+      return `${this.name} serves ${this.cuisineType} food!`;
+    }
+  }
 });
 
 // Your instance + class methods go here
+//Class method
+Restaurant.findBestBy = async function(rating) {
+  const best = await Restaurant.findAll({
+    where: {
+      rating:{
+        [Sequelize.Op.gte]: rating
+      }
+    }
+  })
+  return best;
+}
+
+//Instance method
+Restaurant.prototype.rate = async function (newRating) {
+  this.rating = newRating;
+  await this.save();
+}
 
 // Your hooks go here
+Restaurant.beforeUpdate((restaurant) => {
+  restaurant.numVisits += 1;
+})
+
+Restaurant.beforeCreate((restaurant) => {
+  restaurant.name = restaurant.name.split(' ').map(char => {
+    return char.substring(0,1).toUpperCase() + char.substring(1);
+  }).join(' ');
+})
+//restaurant first
+//[restaurant, first]
+//[R, F]
+
+Restaurant.beforeDestroy((restaurant) => {
+  if(restaurant.cuisineType === 'CHINESE'){
+    throw new Error(' We Like Chinese Food!')
+  }
+})
 
 module.exports = Restaurant;
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,7 +1,30 @@
 const express = require('express');
+const { Restaurant, MenuItem } = require('../models');
 const restaurantsRouter = express.Router();
 
 // Your routes go here
 
+restaurantsRouter.get('/', async (req,res,next) => {
+    const restaurant = await Restaurant.findAll();
+    res.send(restaurant);
+})
+
+restaurantsRouter.get('/:restaurantId', async(req,res,next) => {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId, {
+        include: 'dishes'
+    });
+    res.send(restaurant);
+})
+
+restaurantsRouter.delete('/:id', async (req,res,next) => {
+    try {
+        await Restaurant.destroy({ where: {
+            id: req.params.id
+        }})
+        res.sendStatus(204)
+    } catch (error) {
+        next(error)
+    } 
+})
 
 module.exports = restaurantsRouter;
